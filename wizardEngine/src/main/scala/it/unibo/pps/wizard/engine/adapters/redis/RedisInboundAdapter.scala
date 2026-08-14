eb3cd06dc5cc2f454832ac39a7a9e93e66d196cd
Alex Mazzoni <alexmaz03@hotmail.it>
2026-08-14 22:26:48 +0200

    feat: init game Exception during redis decoding in InboundPort

--- wizardEngine/src/main/scala/it/unibo/pps/wizard/engine/adapters/redis/RedisInboundAdapter.scala
+++ wizardEngine/src/main/scala/it/unibo/pps/wizard/engine/adapters/redis/RedisInboundAdapter.scala
@@ -21,13 +21,18 @@ class RedisInboundAdapter(
     private val outboundPort: OutboundPort
 ) extends InboundPort:
 
+  // todo: related issue https://github.com/pallax03/WizardCardGame/issues/39
   override def getState(lobbyId: LobbyId, playerId: PlayerId): Future[PlayerGameState] =
     val key = ChannelsKeys.game(lobbyId)
     redisClient.send(Request.cmd(Command.GET).arg(key)).asScala.map:
       case null => throw new RuntimeException("Game not found")
       case response => 
-        val serverState = response.toString.decodeAs[ServerGameState].toOption.get // todo: handle Option.get with GameError
-        PlayerGameState.from(serverState, playerId).toOption.get // todo: handle Option.get with GameError
+        val serverState = response.toString.decodeAs[ServerGameState] match
+          case Right(state) => state
+          case Left(err) => throw GameException(GameError.InconsistentState(InconsistentStateReasons.CorruptedState(err.toString)))
+        PlayerGameState.from(serverState, playerId) match
+          case Right(state) => state
+          case Left(err) => throw GameException(err)
 
   override def startGame(lobbyId: LobbyId, players: List[PlayerId], config: GameConfiguration): Future[Unit] =
     val key = ChannelsKeys.game(lobbyId)
@@ -48,7 +53,9 @@ class RedisInboundAdapter(
     redisClient.send(Request.cmd(Command.GET).arg(key)).asScala.flatMap:
       case null => Future.successful(())
       case response =>
-        val oldState = response.toString.decodeAs[ServerGameState].toOption.get
+        val oldState = response.toString.decodeAs[ServerGameState] match
+          case Right(state) => state
+          case Left(err) => throw GameException(GameError.InconsistentState(InconsistentStateReasons.CorruptedState(err.toString)))
         GameEngine.processAction(oldState, action) match
           case Left(error) =>
             outboundPort.publish(lobbyId, FailureEvent.ActionFailed(action.playerId, error))
