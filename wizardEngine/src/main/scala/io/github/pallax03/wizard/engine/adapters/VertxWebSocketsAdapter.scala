e2c33e04a77ab2674dd11f25bd2f7d55cb2e7695
Alex Mazzoni <alexmaz03@hotmail.it>
2026-09-16 15:10:32 +0200

    chore: linting and formatting

--- wizardEngine/src/main/scala/io/github/pallax03/wizard/engine/adapters/VertxWebSocketsAdapter.scala
+++ wizardEngine/src/main/scala/io/github/pallax03/wizard/engine/adapters/VertxWebSocketsAdapter.scala
@@ -66,7 +66,8 @@ class VertxWebSocketsAdapter(
         lobbyStatePort
           .setPlayerOnlineStatus(lobbyId, playerId, true)
           .onComplete:
-            case Success(Some(lobbyStatus: LobbyStatus)) if lobbyStatus != LobbyStatus.PAUSED && lobbyStatus.existGame  =>
+            case Success(Some(lobbyStatus: LobbyStatus))
+                if lobbyStatus != LobbyStatus.PAUSED && lobbyStatus.existGame =>
               gameEngineInPort.resumeGame(lobbyId)
             case _ => ()
         pubSubPort.publish(
--- wizardEngine/src/main/scala/io/github/pallax03/wizard/engine/adapters/redis/RedisLobbyStateAdapter.scala
+++ wizardEngine/src/main/scala/io/github/pallax03/wizard/engine/adapters/redis/RedisLobbyStateAdapter.scala
@@ -2,8 +2,11 @@ package io.github.pallax03.wizard.engine.adapters.redis
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
+
 import cats.syntax.all.*
+
 import io.vertx.redis.client.{Command, Redis, Request}
+
 import io.github.pallax03.wizard.codecs.engine.lobby.LobbyCodecs.given
 import io.github.pallax03.wizard.codecs.engine.model.SystemEventCodecs.given
 import io.github.pallax03.wizard.codecs.syntax.CodecSyntax.*
@@ -107,7 +110,7 @@ class RedisLobbyStateAdapter(redisClient: Redis) extends LobbyStatePort:
       case Left(_)         => Future.successful(None)
       case Right(newLobby) => manageDisconnectTimer(lobbyId, newLobby, isOnline).map(Some(_))
     }
-  
+
   private def manageDisconnectTimer(
       lobbyId: LobbyId,
       lobby: Lobby,
